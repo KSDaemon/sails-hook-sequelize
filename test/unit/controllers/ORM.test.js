4ff920c23432953a434491375c982b7e7732625c
Konstantin Burkalev <burkalev@gollard.ru>
2017-07-26 15:05:08 +0300

    Readded should package to tests

--- test/unit/controllers/ORM.test.js
+++ test/unit/controllers/ORM.test.js
@@ -1,5 +1,5 @@
 // var request = require('supertest');
-// var should = require('should');
+var should = require('should');
 var fixtures = require('./../../fixtures/instances.json');
 
 describe('User model', function () {

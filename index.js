3e634fdb0c4cefff36ce75291e04c5cb6318fa7f
Konstantin Burkalev <KSDaemon@ya.ru>
2017-08-11 17:12:11 +0300

    Pretified logging of exposing models globally

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ module.exports = function (sails) {
                     }
 
                     if (sails.config.globals.models) {
-                        sails.log.verbose('Exposing models globally');
+                        sails.log.verbose('Exposing model \'' + modelDef.globalId + '\' globally');
                         global[modelDef.globalId] = modelClass;
                     }
                     sails.models[modelDef.globalId.toLowerCase()] = modelClass;

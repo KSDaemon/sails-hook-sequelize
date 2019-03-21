2da50be089c67a09efc013d879745ce838531429
Damien Marble <damienm@rainn.org>
2019-03-21 17:17:48 -0400

    feat(shared model defs): Undo erroneously committed local changes

--- test/overall.test.js
+++ test/overall.test.js
@@ -8,27 +8,27 @@ describe('Sails.js Sequelize hook test suite', () => {
         decache('./unit/scope.test');
     };
 
-    // require('./bootstrap.v0.11.test');
-    // clean();
-    //
-    // require('./bootstrap.v0.12-many-schemes.test');
-    // clean();
-    //
-    // require('./bootstrap.v0.12-create-db-schemes.test');
-    // clean();
-    //
-    // // this test should run after bootstrap.v0.12-create-db-schemes because it doesn't create tables
-    // require('./bootstrap.v0.12-migrate-safe.test');
-    // clean();
-    //
-    // require('./bootstrap.v0.12-sqlite.test');
-    // clean();
-    //
-    // require('./bootstrap.v0.12-no-conn.test');
-    // clean();
-    //
-    // require('./bootstrap.v0.12-sequelize-waterline.test');
-    // clean();
+    require('./bootstrap.v0.11.test');
+    clean();
+
+    require('./bootstrap.v0.12-many-schemes.test');
+    clean();
+
+    require('./bootstrap.v0.12-create-db-schemes.test');
+    clean();
+
+    // this test should run after bootstrap.v0.12-create-db-schemes because it doesn't create tables
+    require('./bootstrap.v0.12-migrate-safe.test');
+    clean();
+
+    require('./bootstrap.v0.12-sqlite.test');
+    clean();
+
+    require('./bootstrap.v0.12-no-conn.test');
+    clean();
+
+    require('./bootstrap.v0.12-sequelize-waterline.test');
+    clean();
 
     require('./bootstrap.v1.0.test');
     clean();
